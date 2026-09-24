91d87e8b9e5fc4364d52efe9e3e5145a5681c0d2
KHODJAMBERDIYEV AZIZULLO RAVSHANBEKOVICH <azizullo@KHODJAMBERDIYEVs-MacBook-Air.local>
2026-09-24 16:19:22 +0900

    M task completed

--- src/train.ts
+++ src/train.ts
@@ -1,12 +1,25 @@
-function reverseSentence(str){
-  const words = str.split(" ")
-  let newSentence = "";
-  
-  for(let letter of words){
-    letter = letter.split("").reverse().join("")
-    newSentence = newSentence + " " + letter
-  }
-  return newSentence
+ // TASK M: 
+
+// Shunday function yozing, u raqamlardan tashkil topgan array qabul qilsin 
+// va array ichidagi har bir raqam uchun raqamni ozi va hamda osha raqamni 
+// kvadratidan tashkil topgan object hosil qilib, hosil bolgan objectlarni array ichida qaytarsin.
+// MASALAN: getSquareNumbers([1, 2, 3]) return [{number: 1, square: 1}, {number: 2, square: 4}, {number: 3, square: 9}];
+
+interface Raqam {
+  son: number
+  kvadrat: number
 }
-const result = reverseSentence("we like coding");
-console.log(result)
+
+function getSquareNumbers(ary: number[]): Raqam[] {
+  return ary.map((code) => {
+    return {
+      son: code,
+      kvadrat: code ** 2
+    };
+  });
+}
+
+console.log(getSquareNumbers([1, 2, 3]));
+
+
+
