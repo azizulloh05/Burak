91e430e31e4caed0aeb1d4fc0c5e339ab14b08b7
KHODJAMBERDIYEV AZIZULLO RAVSHANBEKOVICH <azizullo@KHODJAMBERDIYEVs-MacBook-Air.local>
2026-09-24 16:11:35 +0900

    feat:express integrations

---
+++ src/app.ts
@@ -0,0 +1,15 @@
+import express from "express";
+import path from "path";
+
+/** 1-ENTRANCE POINT */
+const app = express();
+app.use(express.static(path.join(__dirname, "public")));
+app.use(express.json());
+/**2- Session */
+
+/**3-Views */
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "ejs");
+/**4- Routers */
+
+export default app;  
