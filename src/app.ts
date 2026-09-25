86ba8a4ae415fb36dc25c6a807e1f9bfa99dac14
KHODJAMBERDIYEV AZIZULLO RAVSHANBEKOVICH <azizullo@KHODJAMBERDIYEVs-MacBook-Air.local>
2026-09-26 00:50:06 +0900

    fix: modify routing system and controllers

--- src/app.ts
+++ src/app.ts
@@ -1,20 +1,22 @@
 import express from "express";
 import path from "path";
 import router from "./router";
+import routerAdmin from "./routerAdmin";
 
 /** 1-ENTRANCE POINT */
 const app = express();
 app.use(express.static(path.join(__dirname, "public")));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+
 /**2- Session */
 
 /**3-Views */
 app.set("views", path.join(__dirname, "views"));
-
 app.set("view engine", "ejs");
-/**4- Routers */
 
-app.use("/", router);   //Middleware design pattern
+/**4- Routers */
+app.use("/admin",routerAdmin); //BSSR: EJS
+app.use("/", router);   // SPA:React
 
 export default app;  
