6e3081b5c0986cfd8b93367fc6c147f5b64786df
KHODJAMBERDIYEV AZIZULLO RAVSHANBEKOVICH <azizullo@KHODJAMBERDIYEVs-MacBook-Air.local>
2026-09-26 00:16:27 +0900

    feat: create routing and member controller

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,6 @@
 import express from "express";
 import path from "path";
+import router from "./router";
 
 /** 1-ENTRANCE POINT */
 const app = express();
@@ -13,5 +14,7 @@ app.set("views", path.join(__dirname, "views"));
 
 app.set("view engine", "ejs");
 /**4- Routers */
+
+app.use("/", router);   //Middleware design pattern
 
 export default app;  
