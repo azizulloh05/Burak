971e9e58041ca74a79eb874531b01e3c2f79d1c7
KHODJAMBERDIYEV AZIZULLO RAVSHANBEKOVICH <azizullo@KHODJAMBERDIYEVs-MacBook-Air.local>
2026-09-26 14:23:03 +0900

    feat:member service model integration

--- src/server.ts
+++ src/server.ts
@@ -4,14 +4,14 @@ dotenv.config();
 import mongoose from "mongoose";
 import app from "./app";
 
-
+mongoose.set("strictQuery", false);
 mongoose
   .connect(process.env.MONGO_URL as string, {})
   .then((data) => {
     console.log("MongoDB connection succeed");
     const PORT = process.env.PORT ?? 3003;
     app.listen(PORT, function () {
-        console.log("Server is running successfully on PORT: " + PORT);
-        });
+        console.log(`Server is running successfully on PORT: ${PORT}`);
+    });
   })
   .catch((err) => console.log("ERROR on connection MongoDB", err));
